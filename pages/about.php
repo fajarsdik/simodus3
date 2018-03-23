14d276c636ff975944139baabaf6c9edefcf91f7
fajarsdik <fajar.sdik@gmail.com>
2018-03-23 09:23:25 +0700

    Buat template about..

---
+++ pages/about.php
@@ -0,0 +1,38 @@
+<?php
+//cek session
+if (empty($_SESSION['admin'])) {
+    echo '<script language="javascript">
+        window.alert("Anda harus login terlebih dahulu!");
+        window.location.href="../index.php"
+    </script>';
+    die();
+} else { ?>
+    
+    <div id = "page-wrapper">
+            <div class = "row">
+                <div class="col-lg-12"> 
+                <div class = "panel">
+                    <div class = "panel-body">
+                        
+                        <h1>Tentang</h1> <hr/><br/>
+                        
+                        <!-- isi konten disini -->
+                        <p>
+                            Konten isi disini bg....
+                            
+                            
+                        </p>
+                        
+                        
+                        
+                        <!-- akhir konten -->
+                        
+                    </div>
+                </div>
+                </div>
+            </div>
+                
+    </div>
+                        
+ <?php   
+}
