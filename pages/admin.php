bfbc1073646d11301c2e348ff4cba946d56e20ff
sayid tarmizi <sayidtarmizi@gmail.com>
2018-03-28 23:38:24 +0700

    menampilkan nama pada dashboar
    request bang widi, menampilkan nama user di web

--- pages/admin.php
+++ pages/admin.php
@@ -72,14 +72,24 @@ if (empty($_SESSION['admin'])) {
                         include "comingsoon.php";
                         break;
                     
+                    
+                    
                 }
             } else {
+                
+                $unit =$_SESSION['unit'];
+                $username =$_SESSION['username'];
+                
                 ?>
 
                 <div id="page-wrapper">
                     <div class="row">
                         <div class="col-lg-12">
                             <h1 class="page-header">Dashboard</h1>
+                            <?php
+                            echo "Selamat datang  ,.$username.";
+                            ?>
+                            
                         </div>
                         <!-- /.col-lg-12 -->
                     </div>
