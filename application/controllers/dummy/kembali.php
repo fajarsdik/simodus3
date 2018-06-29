f5135fc82d02ff52d61e3bcb789ae8ed6a1bc332
sayid tarmizi <sayidtarmizi@gmail.com>
2018-06-29 19:00:50 +0700

    Revert "done untuk kembali"
    This reverts commit 5c53e3ec2540302eee9f2da67a10a8771d6f0791.

--- application/controllers/dummy/kembali.php
+++ application/controllers/dummy/kembali.php
@@ -1,107 +1,74 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-	class Kembali extends MY_Controller {
+	class Aktivasi extends MY_Controller {
 
 		public function __construct(){
 			parent::__construct();
-			$this->load->model('dummy/kembali_model', 'kembali_model');
+			$this->load->model('dummy/aktivasi_model', 'aktivasi_model');
 		}
 
 		public function index(){
-			$data['all_users'] =  $this->kembali_model->get_all_users();
-			$data['view'] = 'dummy/kembali/kembali_list';
+			$data['all_users'] =  $this->aktivasi_model->get_all_users();
+			$data['view'] = 'dummy/aktivasi/aktivasi_list';
 			$this->load->view('admin/layout', $data);
-		
 		}
 		
-		public function add(){
+		public function histori(){
+			$data['histori'] =  $this->aktivasi_model->get_histori();
+			$data['view'] = 'dummy/aktivasi/histori_aktivasi';
+			$this->load->view('admin/layout', $data);
+		}
+
+		public function entri($id){
 			if($this->input->post('submit')){
-
-                                $this->form_validation->set_rules('id_meter', 'Nomor Dummy', 'trim|required');
-                                $this->form_validation->set_rules('nama_cc', 'Nama Call Center', 'trim|required');
-                                $this->form_validation->set_rules('stand', 'Stand Dummy', 'trim|required');
-
+                                $this->form_validation->set_rules('alasan_rusak', 'Alasan Rusak', 'trim|required');
+                                $this->form_validation->set_rules('sisa_pulsa', 'Sisa Pulsa', 'trim|required');
+                                $this->form_validation->set_rules('id_pelanggan', 'IDPEL', 'trim|required');
+                                $this->form_validation->set_rules('no_meter_baru', 'Nomor Meter Baru', 'trim|required');
+                                $this->form_validation->set_rules('nama', 'Nama Petugas Aktivasi', 'trim|required');
+                                
 				if ($this->form_validation->run() == FALSE) {
-					$data['view'] = 'dummy/kembali/kembali_add';
-                                        $data['dummy']= $this->kembali_model->get_dummy_kbl();
+					$data['dummy'] = $this->aktivasi_model->get_dummy_by_id($id);
+					$data['view'] = 'dummy/aktivasi/aktivasi_entri';
 					$this->load->view('admin/layout', $data);
 				}
 				else{
-                                        
-                                        $id = $this->input->post('id_meter');
-                                        $dummy_kembali = $this->kembali_model->get_dummy_by_id_kbl($id);
-                                        $no_dummy=$dummy_kembali['no_dummy'];
 					$data = array(
                                                 'id_meter' => $id,
-                                                'no_dummy' => $no_dummy,
-                                                'stand' => $this->input->post('stand'),
-                                                'nama' => $this->input->post('nama_cc'),
-                                                'unit' => $this->session->userdata('unit'),
+                                                'no_dummy' => $this->input->post('no_dummy'),
+                                                'no_meter_rusak' => $this->input->post('no_meter_rusak'),
+                                                'no_meter_baru' => $this->input->post('no_meter_baru'),
+                                                'id_pelanggan' => $this->input->post('id_pelanggan'),
+                                                'nama' => $this->input->post('nama'),
                                                 'id_user' => $this->session->userdata('admin_id'),
+                                                'unit' => $this->session->userdata('unit'),
 					);
                                         
-                                        $tes='ready';                                            
-                                        $tgl_kembali = date("Y-m-d H:i:s");   
+                                        $edit_pakai = array(
+                                                'alasan_rusak' => $this->input->post('alsan_rusak'),
+                                                'sisa_pulsa' => $this->input->post('sisa_pulsa'),
+                                            
+                                        );
+                                        
+                                        $no_dummy = $this->input->post('no_dummy');
+                                        $tgl_aktivasi = date("Y-m-d H:i:s");   
 					$data_stok = array(
-                                                'tgl_kembali' => $tgl_kembali,
-                                                'posko' => $this->session->userdata('name'),  
-                                                'status'=> $tes,
+                                                'tgl_aktivasi' => $tgl_aktivasi,
+                                                'no_meter_rusak' => $this->input->post('no_meter_baru'),
                                         );
                                         
 					$data = $this->security->xss_clean($data);
-					$result = $this->kembali_model->add_kembali($data,$data_stok,$no_dummy,$id);
-					if($result ){
-						$this->session->set_flashdata('msg', 'Record is Added Successfully!');
-						redirect(base_url('dummy/kembali'));
-					}
-				}
-			}
-			else{
-                                $data['view'] = 'dummy/kembali/kembali_add';
-                                $data['dummy']= $this->kembali_model->get_dummy_kbl();
-                                $this->load->view('admin/layout', $data);
-			}
-			
-		}
-
-		public function edit($id = 0){
-			if($this->input->post('submit')){
-                                $this->form_validation->set_rules('no_meter_rusak', 'Nomor Meter Rusak', 'trim|required');
-                                $this->form_validation->set_rules('alasan_rusak', 'Alasan Rusak', 'trim|required');
-                                $this->form_validation->set_rules('ptgs_pasang', 'Petugas Pasang', 'trim|required');
-                                $this->form_validation->set_rules('sisa_pulsa', 'Sisa Pulsa', 'trim|required');
-                                $this->form_validation->set_rules('no_hp_plg', 'No HP pelanggan', 'trim|required');
-                                $this->form_validation->set_rules('nama_cc', 'Nama Call Center', 'trim|required');
-                                $this->form_validation->set_rules('std_dummy', 'Stand Dummy', 'trim|required');
-                                
-				if ($this->form_validation->run() == FALSE) {
-					$data['dummy'] = $this->pakai_model->get_dummy_by_id($id);
-					$data['view'] = 'dummy/pakai/pakai_edit';
-					$this->load->view('admin/layout', $data);
-				}
-				else{
-					$data = array(
-                                                'no_meter_rusak' => $this->input->post('no_meter_rusak'),
-                                                'alasan_rusak' => $this->input->post('alasan_rusak'),
-                                                'ptgs_pasang' => $this->input->post('ptgs_pasang'),
-                                                'sisa_pulsa' => $this->input->post('sisa_pulsa'),
-                                                'no_hp_plg' => $this->input->post('no_hp_plg'),
-                                                'nama' => $this->session->userdata('name'),
-                                                'id_user' => $this->session->userdata('admin_id'),
-                                                'std_dummy' => $this->input->post('std_dummy'),
-					);
-					$data = $this->security->xss_clean($data);
-					$result = $this->pakai_model->edit_dummy($data, $id);
+					$result = $this->aktivasi_model->entri_model($data,$data_stok,$no_dummy,$id);
 					if($result){
 						$this->session->set_flashdata('msg', 'Record is Updated Successfully!');
-						redirect(base_url('dummy/pakai'));
+						redirect(base_url('dummy/aktivasi'));
 					}
 				}
 			}
 			else{
-				$data['dummy'] = $this->pakai_model->get_dummy_by_id($id);
-				$data['view'] = 'dummy/pakai/pakai_edit';
+				$data['dummy'] = $this->aktivasi_model->get_dummy_by_id($id);
+				$data['view'] = 'dummy/aktivasi/aktivasi_entri';
 				$this->load->view('admin/layout', $data);
 			}
 		}
